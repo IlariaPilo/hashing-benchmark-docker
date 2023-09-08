d291e1f0f95f76e87eece1812d6fad200f43dc57
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-09-08 15:21:49 +0200

    Improve Docker container

--- scripts/setup_datasets.sh
+++ scripts/setup_datasets.sh
@@ -89,4 +89,6 @@ download_dataset "wiki_ts_200M_uint64" $data_dir $url_wiki $check_wiki
 download_dataset "osm_cellids_200M_uint64" $data_dir $url_osm $check_osm
 
 # Patch the /src/support/datasets.hpp file (-i = in place)
-sed -i "s,../data,${data_dir},g" ${BASE_DIR}/code/src/support/datasets.hpp
+if [ $# -lo 2 ] || [ "$2" != "--no-patch" ]; then
+    sed -i "s,../data,${data_dir},g" ${BASE_DIR}/code/src/support/datasets.hpp
+fi
