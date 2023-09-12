2cc6e3ede5e5c690bdbdb7504b910c52275062fc
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-09-12 10:09:37 +0200

    Add one benchmark

---
+++ scripts/one_benchmark.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e          # Stop in case there is an error
+
+INITIAL_DIR=$(pwd)
+_source_dir_=$(dirname "$0")
+BASE_DIR=$(readlink -f "${_source_dir_}/..")     # /home/ilaria/Documents/stage/hashing-benchmark-docker
+
+# go in the code directory
+cd $BASE_DIR/code
+source .env
+
+# Build the target
+./build.sh benchmarks RELEASE
+
+# Get all benchmarks
+readarray -t all_bm < <(cmake-build-release/src/benchmarks --benchmark_list_tests)
+# declare -p all_bm     # Check if the declaration went well
+
+cmake-build-release/src/benchmarks --benchmark_filter="${bm_i[0]}" \
+        --benchmark_out=../output/one_result.json --benchmark_out_format=json
+
