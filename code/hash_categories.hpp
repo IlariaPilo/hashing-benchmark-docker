34c88d4034891ceffd12a9b03dc561dc5c7f6957
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-09-13 10:07:57 +0200

    Fix table and define underscore behaviour

--- code/hash_categories.hpp
+++ code/hash_categories.hpp
@@ -16,7 +16,7 @@ const std::unordered_map<std::string, HashCategories> HASH_FN_TYPES = {
     {"RMIHash", HashCategories::LEARNED},
     {"RadixSplineHash", HashCategories::LEARNED},
     {"PGMHash", HashCategories::LEARNED},
-    {"MurmurFinalizer", HashCategories::CLASSIC},
+    {"MURMUR", HashCategories::CLASSIC},
     {"MultPrime64", HashCategories::CLASSIC},
     {"FibonacciPrime64", HashCategories::CLASSIC},
     {"AquaHash", HashCategories::CLASSIC},
@@ -30,25 +30,33 @@ const std::unordered_map<std::string, HashCategories> HASH_FN_TYPES = {
 // To be called as : if (is_learned<RMIHash>()) ...
 template <typename T>
 bool is_learned() {
-    std::string name = typeid(T).name();
+    std::string full_name = typeid(T).name();
+    size_t _pos_ = full_name.find('_');
+    std::string name = full_name.substr(0, _pos_);
     auto it = HASH_FN_TYPES.find(name);
     return (it != HASH_FN_TYPES.end() && it->second == HashCategories::LEARNED);
 }
 template <typename T>
 bool is_classic() {
-    std::string name = typeid(T).name();
+    std::string full_name = typeid(T).name();
+    size_t _pos_ = full_name.find('_');
+    std::string name = full_name.substr(0, _pos_);
     auto it = HASH_FN_TYPES.find(name);
     return (it != HASH_FN_TYPES.end() && it->second == HashCategories::CLASSIC);
 }
 template <typename T>
 bool is_perfect() {
-    std::string name = typeid(T).name();
+    std::string full_name = typeid(T).name();
+    size_t _pos_ = full_name.find('_');
+    std::string name = full_name.substr(0, _pos_);
     auto it = HASH_FN_TYPES.find(name);
     return (it != HASH_FN_TYPES.end() && it->second == HashCategories::PERFECT);
 }
 template <typename T>
 HashCategories get_fn_type() {
-    std::string name = typeid(T).name();
+    std::string full_name = typeid(T).name();
+    size_t _pos_ = full_name.find('_');
+    std::string name = full_name.substr(0, _pos_);
     auto it = HASH_FN_TYPES.find(name);
     if (it == HASH_FN_TYPES.end())
         return HashCategories::UNKNOWN;
